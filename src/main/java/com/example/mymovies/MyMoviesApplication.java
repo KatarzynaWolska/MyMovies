24b295ef2593511bf457576b231441eebac02956
KatarzynaWolska <kasia.wolska137@gmail.com>
2020-04-01 14:59:04 +0200

    Setup project

---
+++ src/main/java/com/example/mymovies/MyMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMoviesApplication.class, args);
+    }
+
+}
